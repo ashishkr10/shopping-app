338744ec0c63bf2cd88bde8b11cab227d1427214
Ashish <kr.aashish10@gmail.com>
2021-12-24 20:40:34 +0530

    Shopping app updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => {
     <>
       <footer
         id="sticky-footer"
-        class="flex-shrink-0 py-3 bg-dark text-white-50"
+        className="flex-shrink-0 py-3 bg-dark text-white-50"
       >
-        <div class="container text-center">
+        <div className="container text-center">
           <small>&copy; 2022 Copyright | ShoppingMart.com</small>
         </div>
       </footer>
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,12 +5,15 @@ import DATA from "../Data";
 const Product = () => {
   const cardItem = (item) => {
     return (
-      <div class="card my-5 py-4" key={item.id} style={{ width: "18rem" }}>
-        <img src={item.img} class="card-img-top" alt={item.title} />
-        <div class="card-body text-center">
-          <h5 class="card-title">{item.title}</h5>
+      <div className="card my-5 py-4" key={item.id} style={{ width: "18rem" }}>
+        <img src={item.img} className="card-img-top" alt={item.title} />
+        <div className="card-body text-center">
+          <h5 className="card-title">{item.title}</h5>
           <p className="lead">$ {item.price}</p>
-          <NavLink to={`/products/${item.id}`} class="btn btn-outline-primary">
+          <NavLink
+            to={`/products/${item.id}`}
+            className="btn btn-outline-primary"
+          >
             Buy Now
           </NavLink>
         </div>
